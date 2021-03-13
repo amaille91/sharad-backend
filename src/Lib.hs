334dad0d8d7a704edfa53df74e4a9ba00c84be7c
Alexandre Maille <al.maille@laposte.net>
2021-03-13 20:50:35 +0100

    serving static files under the ./static directory

--- src/Lib.hs
+++ src/Lib.hs
@@ -2,7 +2,14 @@ module Lib
     ( runApp
     ) where
 
-import Happstack.Server (nullConf, simpleHTTP, toResponse, ok)
+import Control.Monad (msum)
+import Control.Monad.IO.Class (liftIO)
+import Happstack.Server (anyPath, serveFileFrom, guessContentTypeM, mimeTypes, uriRest, nullConf, simpleHTTP, toResponse,  method, ok, dir, Method(GET), Conf(..))
 
 runApp :: IO ()
-runApp = simpleHTTP nullConf $ ok "Hello, World!"
+runApp = do
+    simpleHTTP nullConf { port = 8081 } $ do
+        method GET
+        uriRest (\('/':filePath) -> do
+            liftIO $ putStrLn filePath
+            serveFileFrom "static/" (guessContentTypeM mimeTypes) filePath)
