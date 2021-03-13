049ce5ead216f09842b5471ea31c3ae147574d3e
Alexandre Maille <al.maille@laposte.net>
2021-03-13 23:21:47 +0100

    getting body string from POST /note

--- src/Lib.hs
+++ src/Lib.hs
@@ -2,14 +2,57 @@ module Lib
     ( runApp
     ) where
 
-import Control.Monad (msum)
+import Control.Monad (msum, mzero)
 import Control.Monad.IO.Class (liftIO)
-import Happstack.Server (anyPath, serveFileFrom, guessContentTypeM, mimeTypes, uriRest, nullConf, simpleHTTP, toResponse,  method, ok, dir, Method(GET), Conf(..))
+import Control.Concurrent.MVar (takeMVar)
+import qualified Data.ByteString.Lazy.Char8 as BL (unpack)
+import Happstack.Server (Response, ServerPartT, BodyPolicy, RqBody, takeRequestBody, unBody, rqBody, decodeBody, askRq, defaultBodyPolicy, toResponse, nullDir, path, serveFileFrom, guessContentTypeM, mimeTypes, uriRest, nullConf, simpleHTTP, toResponse,  method, ok, dir, Method(GET, POST), Conf(..))
 
 runApp :: IO ()
 runApp = do
-    simpleHTTP nullConf { port = 8081 } $ do
-        method GET
-        uriRest (\('/':filePath) -> do
-            liftIO $ putStrLn filePath
-            serveFileFrom "static/" (guessContentTypeM mimeTypes) filePath)
+    simpleHTTP nullConf { port = 8081 } $ msum [ noteController
+                                               ,serveStaticResource
+                                               , mzero
+                                               ]
+        
+
+noteController :: ServerPartT IO Response
+noteController = do
+    dir "note" $ do
+        msum [ getNote
+             , postNote
+             , deleteNote
+             , putNote
+             ]
+
+getNote :: ServerPartT IO Response
+getNote = mzero
+
+postNote :: ServerPartT IO Response
+postNote = do
+    nullDir
+    method POST
+    req <- askRq
+    body <- liftIO $ takeRequestBody req
+    case body of
+        Just rqBody -> handleBody rqBody
+        Nothing -> ok $ toResponse "NoBody"
+    where
+        handleBody :: RqBody -> ServerPartT IO Response
+        handleBody rqBody = do
+            let bodyStr = BL.unpack $ unBody rqBody
+            ok $ toResponse bodyStr
+
+deleteNote :: ServerPartT IO Response
+deleteNote = mzero
+
+putNote :: ServerPartT IO Response
+putNote = mzero
+
+
+serveStaticResource :: ServerPartT IO Response
+serveStaticResource = do
+    method GET
+    path (\filePath -> do
+        nullDir
+        serveFileFrom "static/" (guessContentTypeM mimeTypes) filePath)
