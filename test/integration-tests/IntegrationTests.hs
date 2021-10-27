858de0caa6f5d195d4a62cce403fef1070993be4
Alexandre Maille <blank_email>
2021-10-27 11:21:51 +0200

    integration tests compile

--- test/integration-tests/IntegrationTests.hs
+++ test/integration-tests/IntegrationTests.hs
@@ -1,7 +1,10 @@
 {-# LANGUAGE FlexibleContexts    #-}
+{-# LANGUAGE FlexibleInstances    #-}
 {-# LANGUAGE OverloadedStrings   #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE AllowAmbiguousTypes #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE FunctionalDependencies #-}
 
 module IntegrationTests (runIntegrationTests) where
 
@@ -17,6 +20,11 @@ import           Test.Hspec
 import           Test.HUnit
 import Model
 
+-- ===================== Constants ==============================
+
+noteEndpoint = "/note"
+checklistEndpoint = "/checklist"
+
 runIntegrationTests :: IO ()
 runIntegrationTests = hspec $ do
   describe "Integration Tests" $ do
@@ -24,19 +32,20 @@ runIntegrationTests = hspec $ do
       let
         firstNoteContent = NoteContent { title = Just "First note", noteContent = "First note content" }
         firstNoteNewContent = NoteContent { title = Just "First note new title", noteContent = "This is a new content for the first note" }
-      assertServerIsNew
-      createNewContent firstNoteContent
-      [firstNote :: Identifiable NoteContent] <- assertGetWithContent firstNoteContent
-      modify $ Identifiable (storageId firstNote) firstNoteNewContent
-      [modifiedNote] <- assertGetWithContent firstNoteNewContent
-      deleteNote $ (id.storageId) modifiedNote
-      assertServerIsNew
+      assertNoItemAtEndpoint NoteEndpoint
+      createNewContent NoteEndpoint firstNoteContent
+      [firstNote :: Identifiable NoteContent] <- assertGetWithContent NoteEndpoint firstNoteContent
+      modifyItem NoteEndpoint $ Identifiable (storageId firstNote) firstNoteNewContent
+      [modifiedNote] <- assertGetWithContent NoteEndpoint firstNoteNewContent
+      deleteItem noteEndpoint $ (id.storageId) modifiedNote
+      assertNoItemAtEndpoint NoteEndpoint
     it "should satisfy the basics, in one session of the Very Firsr User's needs, checklist-wise" $ do
-      createNewContent firstChecklistContent
-      [firstChecklist :: Identifiable ChecklistContent] <- assertGetWithContent firstChecklistContent
-      modify $ Identifiable (storageId firstChecklist) firstChecklistNewContent
-      [modifiedChecklist :: Identifiable ChecklistContent] <- assertGetWithContent  firstChecklistNewContent
-      deleteChecklist $ (id.storageId) modifiedChecklist
+      assertNoItemAtEndpoint ChecklistEndpoint
+      createNewContent ChecklistEndpoint firstChecklistContent
+      [firstChecklist :: Identifiable ChecklistContent] <- assertGetWithContent ChecklistEndpoint firstChecklistContent
+      modifyItem ChecklistEndpoint $ Identifiable (storageId firstChecklist) firstChecklistNewContent
+      [modifiedChecklist :: Identifiable ChecklistContent] <- assertGetWithContent ChecklistEndpoint firstChecklistNewContent
+      deleteItem checklistEndpoint $ (id.storageId) modifiedChecklist
         where
         firstChecklistContent    = ChecklistContent { name = "First checklist"
                                                      , items = [ ChecklistItem { label = "First item label unchecked", checked = False }
@@ -49,28 +58,25 @@ runIntegrationTests = hspec $ do
                                                               ]
                                                     }
 
-assertServerIsNew :: Expectation
-assertServerIsNew = do
-  getResponse :: Response [Identifiable NoteContent] <- sendRequestWithJSONBody "GET" ()
+assertNoItemAtEndpoint :: (Content contentType, RequestType GET endpointType () [Identifiable contentType]) => endpointType -> Expectation
+assertNoItemAtEndpoint endpoint = do
+  getResponse :: Response [Identifiable contentType] <- sendRequestWithJSONBodyImpl GET endpoint ()
   assertNoNoteInResponse "Failed to start with an empty server" getResponse
 
-createNewContent :: (Content contentType) => contentType -> Expectation
-createNewContent content = do
-  postResponse :: Response StorageId <- sendRequestWithJSONBody "POST" content
+createNewContent :: (Content contentType, RequestType POST endpointType contentType StorageId) => endpointType -> contentType -> Expectation
+createNewContent endpoint content = do
+  postResponse :: Response StorageId <- sendRequestWithJSONBodyImpl POST endpoint content
   assertStatusCode200 ("Failed to create item" ++ show content) postResponse
 
-assertGetWithContent :: Content a => a -> IO [Identifiable a]
-assertGetWithContent expectedContent = do
-  getResponse <- sendRequestWithJSONBody "GET" ()
+assertGetWithContent :: (Content contentType, RequestType GET endpointType () [Identifiable contentType]) => endpointType -> contentType -> IO [Identifiable contentType]
+assertGetWithContent endpoint expectedContent = do
+  getResponse <- sendRequestWithJSONBodyImpl GET endpoint ()
   assertWithFoundContent ("Failed to retrieve created content " ++ show expectedContent) [expectedContent] getResponse
 
-modify :: Content a => Identifiable a -> Expectation
-modify update = do
-  putResponse :: Response StorageId <- sendRequestWithJSONBody "PUT" update
+modifyItem :: (Content contentType, RequestType PUT endpointType (Identifiable contentType) StorageId) => endpointType -> Identifiable contentType -> Expectation
+modifyItem endpoint update = do
+  putResponse :: Response StorageId <- sendRequestWithJSONBodyImpl PUT endpoint update
   assertStatusCode200 ("Failed to apply modification " ++ show update) putResponse
-
-deleteNote = deleteItem "note/"
-deleteChecklist = deleteItem "checklist/"
 
 deleteItem :: String -> String -> Expectation
 deleteItem rootItemPath idToDelete = do
@@ -85,18 +91,23 @@ assertWithFoundContent errorPrefix expectedContents response = do
   where
     responseItems = getResponseBody response
 
-sendRequestWithJSONBody :: (ToJSON requestType, FromJSON responseType) =>
-  ByteString -> requestType -> IO (Response responseType)
-sendRequestWithJSONBody method body = httpJSON $
-  setRequestBodyJSON body $
-    setRequestHeader "Content-Type" ["application/json"] $
-      setRequestMethod method "http://localhost:8081/note"
+sendRequestWithJSONBodyImpl :: (RequestType methodType endpointType requestType responseType) =>
+  methodType -> endpointType -> requestType -> IO (Response responseType)
+sendRequestWithJSONBodyImpl method endpoint body = do
+    req <- parseRequest ("http://localhost:8081" ++ getEndpoint endpoint) 
+    httpJSON $ (setRequestMethod (getMethod method) . (setRequestHeader "Content-Type" ["application/json"]) . (setRequestBodyJSON body)) req
 
-sendRequestSimple :: (ToJSON requestType) => ByteString -> requestType -> IO (Response ByteString)
-sendRequestSimple method body = httpBS $
-  setRequestBodyJSON body $
-    setRequestHeader "Content-Type" ["application/json"] $
-      setRequestMethod method "http://localhost:8081/note"
+
+-- sendRequestWithJSONBodyImpl endpoint method body = httpJSON <$>
+--   setRequestBodyJSON body <$>
+--     setRequestHeader "Content-Type" ["application/json"] <$>
+--       setRequestMethod method <$> parseRequest ("http://localhost:8081" ++ endpoint) 
+-- 
+-- sendRequestSimple :: (ToJSON requestType) => ByteString -> requestType -> IO (Response ByteString)
+-- sendRequestSimple method body = httpBS $
+--   setRequestBodyJSON body $
+--     setRequestHeader "Content-Type" ["application/json"] $
+--       setRequestMethod method "http://localhost:8081/note"
 
 assertNoNoteInResponse errorPrefix response = do
   assertStatusCode200 errorPrefix response
@@ -108,4 +119,54 @@ assertStatusCode200 errorPrefix response = assertEqual (errorPrefix ++ "Expected
 modifyNote :: Content a => a -> Identifiable a -> Identifiable a
 modifyNote newContent previousNote = Identifiable (storageId previousNote) newContent
 
+class Method a where
+    getMethod :: a -> ByteString
 
+data GET = GET
+data POST = POST
+data PUT = PUT
+data DELETE = DELETE
+
+instance Method GET where
+    getMethod GET = "GET"
+
+instance Method POST where
+    getMethod POST = "POST"
+
+instance Method DELETE where
+    getMethod DELETE = "DELETE"
+
+instance Method PUT where
+    getMethod PUT = "PUT"
+
+class Endpoint a where
+    getEndpoint :: a -> String
+
+data NoteEndpoint = NoteEndpoint
+instance Endpoint NoteEndpoint where
+    getEndpoint NoteEndpoint = "/note"
+
+data ChecklistEndpoint = ChecklistEndpoint
+instance Endpoint ChecklistEndpoint where
+    getEndpoint ChecklistEndpoint = "/checklist"
+
+class (ToJSON requestType, FromJSON responseType, Endpoint endpoint, Method methodType) => RequestType methodType endpoint requestType responseType | endpoint methodType -> requestType, endpoint methodType requestType -> responseType where
+    sendRequestWithJSONBody :: endpoint -> methodType -> requestType -> IO (Response responseType)
+
+instance RequestType GET NoteEndpoint () [Identifiable NoteContent] where
+    sendRequestWithJSONBody endpoint _ req = sendRequestWithJSONBodyImpl GET endpoint req
+
+instance RequestType POST NoteEndpoint NoteContent StorageId where
+    sendRequestWithJSONBody endpoint _ req = sendRequestWithJSONBodyImpl POST endpoint req
+
+instance RequestType PUT NoteEndpoint (Identifiable NoteContent) StorageId where
+    sendRequestWithJSONBody endpoint _ req = sendRequestWithJSONBodyImpl PUT endpoint req
+
+instance RequestType GET ChecklistEndpoint () [Identifiable ChecklistContent] where
+    sendRequestWithJSONBody endpoint _ req = sendRequestWithJSONBodyImpl GET endpoint req
+
+instance RequestType POST ChecklistEndpoint ChecklistContent StorageId where
+    sendRequestWithJSONBody endpoint _ req = sendRequestWithJSONBodyImpl POST endpoint req
+
+instance RequestType PUT ChecklistEndpoint (Identifiable ChecklistContent) StorageId where
+    sendRequestWithJSONBody endpoint _ req = sendRequestWithJSONBodyImpl PUT endpoint req
